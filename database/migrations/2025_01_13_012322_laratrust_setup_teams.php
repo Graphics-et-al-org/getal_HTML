782205e53c91a9ba63a461c52ad2e6e4b3e7c3dd
Adam landow <adam_landow@yahoo.com.au>
2025-01-30 11:26:25 +1100

    migration update

--- database/migrations/2025_01_13_012322_laratrust_setup_teams.php
+++ database/migrations/2025_01_13_012322_laratrust_setup_teams.php
@@ -24,7 +24,7 @@ class LaratrustSetupTeams extends Migration
 
         Schema::table('role_user', function (Blueprint $table) {
             // Step 1: Add a temporary primary key
-            $table->bigIncrements('temp_id')->unique(); // Ensure table always has a primary key
+            $table->bigInteger('temp_id')->unsigned()->autoIncrement();
             // Drop role foreign key and primary key
             $table->dropForeign(['role_id']);
             $table->dropPrimary(['user_id', 'role_id', 'user_type']);
